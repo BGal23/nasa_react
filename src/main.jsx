633fdcec8f3b4939bef1658f98b43a9e06667e14
Bartłomiej Gal <bgal23@bartomiejs-mbp.home>
2024-01-03 14:51:50 +0100

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-//import React from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
@@ -7,11 +7,11 @@ import store from "./redux/store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  //<React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename="/nasa_react">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
